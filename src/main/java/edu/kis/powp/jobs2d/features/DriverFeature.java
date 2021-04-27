5ee13c7cc8849f94a9280bad567c6b816c01d1d4
Wiktor Kania <roller47@wp.pl>
2021-04-27 21:53:29 +0200

    removed subscriber and publisher implementations

--- src/main/java/edu/kis/powp/jobs2d/features/DriverFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/DriverFeature.java
@@ -1,20 +1,14 @@
 package edu.kis.powp.jobs2d.features;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.SelectDriverMenuOptionListener;
-import edu.kis.powp.jobs2d.drivers.observer.IPublisher;
-import edu.kis.powp.jobs2d.drivers.observer.ISubscriber;
 
-public class DriverFeature implements IPublisher {
+public class DriverFeature {
 
     private static DriverManager driverManager = new DriverManager();
     private static Application app;
-    private static List<ISubscriber> subscribers = new ArrayList<>();
 
     public static DriverManager getDriverManager() {
         return driverManager;
@@ -39,24 +33,6 @@ public class DriverFeature implements IPublisher {
     public static void addDriver(String name, Job2dDriver driver) {
         SelectDriverMenuOptionListener listener = new SelectDriverMenuOptionListener(driver, driverManager);
         app.addComponentMenuElement(DriverFeature.class, name, listener);
-    }
-
-    public void addSubscriber(ISubscriber subscriber) {
-        subscribers.add(subscriber);
-    }
-
-    public ISubscriber[] getSubscribers() {
-        return subscribers.toArray(new ISubscriber[0]);
-    }
-
-    public void notifyObservers() {
-        for (ISubscriber subscriber : subscribers) {
-            subscriber.update(driverManager.getCurrentDriver().toString());
-        }
-    }
-
-    public void clearObservers() {
-        subscribers.clear();
     }
 
     /**
