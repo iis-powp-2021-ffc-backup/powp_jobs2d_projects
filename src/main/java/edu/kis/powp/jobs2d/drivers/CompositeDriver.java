95c9a2378d5fdeed7c6297974681f3e84e9f9f76
Bartek306 <shwdp13@wp.pl>
2021-04-28 19:48:59 +0200

    commit

--- src/main/java/edu/kis/powp/jobs2d/drivers/CompositeDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/CompositeDriver.java
@@ -8,7 +8,11 @@ import java.util.List;
 public class CompositeDriver implements Job2dDriver {
 
     private List<Job2dDriver> drivers;
-
+    private int setOperations = 0;
+    private int allOperations = 0;
+    private int startX = 0;
+    private int startY = 0;
+    private float distance = 0;
     public CompositeDriver(){
         drivers = new ArrayList<>();
     }
@@ -16,23 +20,37 @@ public class CompositeDriver implements Job2dDriver {
     @Override
     public void setPosition(int x, int y) {
         drivers.forEach(e->e.setPosition(x, y));
+        this.allOperations ++;
     }
 
     @Override
     public void operateTo(int x, int y) {
         drivers.forEach(e->e.operateTo(x, y));
+        this.allOperations ++;
     }
 
     public void add(Job2dDriver driver){
         drivers.add(driver);
     }
 
-    public List getChildren(){
-        return drivers;
+    public Job2dDriver[] getChildren(){
+        return drivers.toArray(new Job2dDriver[drivers.size()]);
     }
 
     public void remove(Job2dDriver children){
         drivers.remove(children);
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public int getAllOperations() {
+        return allOperations;
+    }
+
+    public int getSetOperations() {
+        return setOperations;
     }
 
     @Override
